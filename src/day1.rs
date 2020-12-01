092fc70cc39b6ddd837b1018c1bf82c7254b879c
jaburns <j@jaburns.net>
2020-12-01 10:12:28 -0700

    Generalize day 1

--- src/day1.rs
+++ src/day1.rs
@@ -29,6 +29,29 @@ fn product_of_3_nums_adding_to(sum: i32, nums: &[i32]) -> i32 {
     panic!();
 }
 
+fn product_of_nums_adding_to(sum: i32, num_count: usize, nums: &[i32]) -> i32 {
+    let mut indices = (0..num_count).rev().collect::<Vec<usize>>();
+
+    'top: loop {
+        for i in (0..num_count).rev() {
+            if i == 0 && indices[i] < nums.len() || indices[i] < indices[i - 1] {
+                let sum_test = indices.iter().map(|&x| nums[x]).fold(0i32, |a,b| a + b);
+                if sum_test == sum {
+                    return indices.iter().map(|&x| nums[x]).fold(1i32, |a,b| a * b);
+                }
+                indices[i] += 1;
+                continue 'top;
+            } else if i == 0 {
+                break 'top;
+            } else {
+                indices[i] = 0;
+            }
+        }
+    }
+
+    panic!()
+}
+
 pub fn main() {
     let entries: Vec<i32> = std::fs::read_to_string("data/day1.txt")
         .unwrap()
@@ -38,6 +61,9 @@ pub fn main() {
 
     let part1 = product_of_2_nums_adding_to(2020, entries.as_slice());
     let part2 = product_of_3_nums_adding_to(2020, entries.as_slice());
+    let part1x = product_of_nums_adding_to(2020, 2, entries.as_slice());
+    let part2x = product_of_nums_adding_to(2020, 3, entries.as_slice());
 
     println!("{} {}", part1, part2);
+    println!("{} {}", part1x, part2x);
 }
