c6fcb92420b35f7a82bd2e193c0a73dabec7090f
jaburns <j@jaburns.net>
2020-12-01 10:12:50 -0700

    Format

--- src/day1.rs
+++ src/day1.rs
@@ -35,9 +35,9 @@ fn product_of_nums_adding_to(sum: i32, num_count: usize, nums: &[i32]) -> i32 {
     'top: loop {
         for i in (0..num_count).rev() {
             if i == 0 && indices[i] < nums.len() || indices[i] < indices[i - 1] {
-                let sum_test = indices.iter().map(|&x| nums[x]).fold(0i32, |a,b| a + b);
+                let sum_test = indices.iter().map(|&x| nums[x]).fold(0i32, |a, b| a + b);
                 if sum_test == sum {
-                    return indices.iter().map(|&x| nums[x]).fold(1i32, |a,b| a * b);
+                    return indices.iter().map(|&x| nums[x]).fold(1i32, |a, b| a * b);
                 }
                 indices[i] += 1;
                 continue 'top;
