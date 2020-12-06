389c02b50476fe8e3445165fcd40824c9d8f2739
jaburns <j@jaburns.net>
2020-12-06 15:32:17 -0700

    Simplify day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,43 +1,36 @@
-use std::collections::HashMap;
-
-fn chars_in_all_lines(lines: &Vec<String>) -> usize {
-    let mut answers = HashMap::<char, usize>::new();
-
-    for line in lines {
-        for ch in line.chars() {
-            if !answers.contains_key(&ch) {
-                answers.insert(ch, 1);
-            } else {
-                *answers.get_mut(&ch).unwrap() += 1;
-            }
-        }
-    }
-
-    answers
-        .iter()
-        .fold(0usize, |acc, (_, &v)| acc + (v == lines.len()) as usize)
-}
-
 pub fn main() {
+    // Parse input in to vec of pairs for each group:
+    //     ( Number of lines, all lines joined in to single string )
     let groups = std::fs::read_to_string("data/day6.txt")
         .unwrap()
         .split("\n\n")
-        .map(|x| x.split("\n").map(String::from).collect::<Vec<_>>())
+        .map(|x| {
+            (
+                x.split("\n").collect::<Vec<_>>().len(),
+                String::from(x.replace("\n", "")),
+            )
+        })
         .collect::<Vec<_>>();
 
+    // Sum all of the unique characters in each group.
     let part1 = groups
         .iter()
-        .map(|x| {
-            let mut chs = x.iter().map(|x| x.chars()).flatten().collect::<Vec<char>>();
-            chs.sort();
-            chs.dedup();
-            chs.len()
+        .map(|(_, string)| {
+            let mut x = string.chars().collect::<Vec<_>>();
+            x.sort();
+            x.dedup();
+            x.len()
         })
         .sum::<usize>();
 
+    // Sum the number of characters whose count equals the line count for each group.
     let part2 = groups
         .iter()
-        .map(chars_in_all_lines)
+        .map(|(lines, string)| {
+            ('a'..='z')
+                .filter(|x| string.matches(*x).count() == *lines)
+                .count()
+        })
         .sum::<usize>();
 
     println!("{} {}", part1, part2);
