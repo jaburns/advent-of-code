ce990966c71d7ee949177570dd45e58d477c5a61
jaburns <j@jaburns.net>
2020-12-09 21:59:53 -0700

    Simpler template file

--- scaffold.py
+++ scaffold.py
@@ -6,21 +6,14 @@ import os.path
 from os import path
 
 RUST_DAY_TEMPLATE = """
-fn parse_line(line: &str) -> String {
-    String::from(line)
-}
-
 pub fn main() {
-    let entries: Vec<_> = std::fs::read_to_string("data/day$DAY.txt")
+    let lines = std::fs::read_to_string("data/day$DAY.txt")
         .unwrap()
         .lines()
-        .map(|x| parse_line(x.trim()))
-        .collect();
+        .map(String::from)
+        .collect::<Vec<_>>();
 
-    let part1 = &entries[0];
-    let part2 = &entries[1];
-
-    println!("{} {}", part1, part2);
+    println!("{:?}", lines);
 }
 """
 
