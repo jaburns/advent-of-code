a2677c8330418a3a69325cbe8f827807e2e8a10c
jaburns <j@jaburns.net>
2020-12-13 14:40:14 -0700

    Clean up day 13

--- src/day13.rs
+++ src/day13.rs
@@ -1,10 +1,12 @@
 use modinverse::egcd;
 use num::integer::{lcm, mod_floor};
 
-fn find_earliest_available_bus_solution(departure_time: i64, buses: &[i64]) -> i64 {
+type IndexedBus = (i64, i64);
+
+fn find_earliest_available_bus_solution(departure_time: i64, buses: &[IndexedBus]) -> i64 {
     let (best_bus, wait_time) = buses
         .iter()
-        .map(|x| (x, x - departure_time % x))
+        .map(|(_, x)| (x, x - departure_time % x))
         .min_by(|(_, b0), (_, b1)| b0.cmp(b1))
         .unwrap();
 
@@ -22,11 +24,11 @@ fn synchronize_steps(a: i64, b: i64, b_offset: i64) -> (i64, i64) {
     (mod_floor(-phase, period), lcm(a, b))
 }
 
-fn find_earliest_time_departures_match_indices(indexed_buses: &[(i64, i64)]) -> i64 {
-    let (index0, bus0) = indexed_buses[0];
+fn find_earliest_time_departures_match_indices(buses: &[IndexedBus]) -> i64 {
+    let (index0, bus0) = buses[0];
     let (mut offset, mut period) = (0i64, bus0);
 
-    for &(index, bus) in &indexed_buses[1..] {
+    for &(index, bus) in &buses[1..] {
         let (offset1, period1) = synchronize_steps(period, bus, -(index - index0 + offset));
         offset += offset1;
         period = period1;
@@ -44,25 +46,16 @@ pub fn main() {
 
     let departure_time = lines[0].parse::<i64>().unwrap();
 
-    let just_buses = lines[1]
-        .split(",")
-        .filter_map(|x| x.parse::<i64>().ok())
-        .collect::<Vec<_>>();
-
     let indexed_buses = lines[1]
         .split(",")
         .enumerate()
-        .map(|(i, x)| (i, x.parse::<i64>()))
-        .filter_map(|(i, x)| {
-            if x.is_ok() {
-                Some((i as i64, x.unwrap()))
-            } else {
-                None
-            }
+        .filter_map(|(i, x)| match x.parse::<i64>() {
+            Ok(num) => Some((i as i64, num)),
+            _ => None,
         })
         .collect::<Vec<_>>();
 
-    let part1 = find_earliest_available_bus_solution(departure_time, just_buses.as_slice());
+    let part1 = find_earliest_available_bus_solution(departure_time, indexed_buses.as_slice());
     let part2 = find_earliest_time_departures_match_indices(indexed_buses.as_slice());
 
     println!("{} {}", part1, part2);
