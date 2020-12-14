85ba35919258b851d540a5362b6893753302e295
jaburns <j@jaburns.net>
2020-12-14 10:12:54 -0700

    Day 14 part 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+const MAX_BITS: u64 = 36;
+
 #[derive(Debug, Clone, Copy)]
 enum Instruction {
     SetMask(u64, u64),
@@ -43,6 +45,12 @@ struct Machine {
     memory: HashMap<u64, u64>,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum MachineMode {
+    ValueMask,
+    MemoryMask,
+}
+
 impl Machine {
     pub fn new() -> Self {
         Self {
@@ -52,22 +60,65 @@ impl Machine {
         }
     }
 
-    pub fn run_instruction(&mut self, instruction: Instruction) {
+    pub fn run_instruction(&mut self, instruction: Instruction, mode: MachineMode) {
         match instruction {
             Instruction::SetMask(set, values) => {
                 self.mask_set = set;
                 self.mask_values = values;
             }
-            Instruction::SetMem(addr, val) => {
-                self.memory
-                    .insert(addr, val & !self.mask_set | self.mask_values);
-            }
+            Instruction::SetMem(addr, val) => match mode {
+                MachineMode::ValueMask => self.update_with_value_mask(addr, val),
+                MachineMode::MemoryMask => self.update_with_memory_mask(addr, val),
+            },
         };
+    }
+
+    fn update_with_value_mask(&mut self, addr: u64, val: u64) {
+        self.memory
+            .insert(addr, val & !self.mask_set | self.mask_values);
+    }
+
+    fn update_with_memory_mask(&mut self, mut addr: u64, val: u64) {
+        for i in 0..MAX_BITS {
+            let bit = 1 << i;
+            if self.mask_set & bit == 0 {
+                addr &= !bit;
+            } else if self.mask_values & bit != 0 {
+                addr |= bit;
+            }
+        }
+
+        let mut write_addrs = Vec::<u64>::new();
+
+        for i in 0..MAX_BITS {
+            let bit = 1 << i;
+            if self.mask_set & bit == 0 {
+                if write_addrs.len() == 0 {
+                    write_addrs.push(addr);
+                    addr |= bit;
+                    write_addrs.push(addr);
+                } else {
+                    write_addrs.extend(write_addrs.iter().map(|x| x | bit).collect::<Vec<_>>());
+                }
+            }
+        }
+
+        for wa in write_addrs {
+            self.memory.insert(wa, val);
+        }
     }
 
     pub fn memory_sum(&self) -> u64 {
         self.memory.values().sum()
     }
+}
+
+fn run_instructions_and_sum_memory(instructions: &[Instruction], mode: MachineMode) -> u64 {
+    let mut machine = Machine::new();
+    for &ins in instructions {
+        machine.run_instruction(ins, mode);
+    }
+    machine.memory_sum()
 }
 
 pub fn main() {
@@ -77,13 +128,8 @@ pub fn main() {
         .map(Instruction::parse)
         .collect::<Vec<_>>();
 
-    let part1 = {
-        let mut machine = Machine::new();
-        for ins in instructions {
-            machine.run_instruction(ins);
-        }
-        machine.memory_sum()
-    };
+    let part1 = run_instructions_and_sum_memory(&instructions, MachineMode::ValueMask);
+    let part2 = run_instructions_and_sum_memory(&instructions, MachineMode::MemoryMask);
 
-    println!("{}", part1);
+    println!("{} {}", part1, part2);
 }
