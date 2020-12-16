c539594f590ca548d45eb2b9dbbb7e3924b320c1
jaburns <j@jaburns.net>
2020-12-15 22:35:46 -0700

    Much faster day 15

--- src/day15.rs
+++ src/day15.rs
@@ -1,26 +1,22 @@
-use std::collections::HashMap;
+const MAX_ITERATIONS: usize = 30_000_000;
 
-fn run_game(iterations: usize, start_data: &[usize], show_log: bool) -> usize {
-    let mut turns_for_nums = HashMap::<usize, usize>::new();
-    let mut turn = start_data.len();
+fn run_game(iterations: usize, start_data: &[u32]) -> u32 {
+    let mut turns_for_nums = vec![std::u32::MAX; MAX_ITERATIONS];
+
+    let mut turn = start_data.len() as u32;
     let mut prev = *start_data.last().unwrap();
-    let log_rate = iterations / 100;
 
     for i in 0..(start_data.len() - 1) {
-        turns_for_nums.insert(start_data[i], i);
+        turns_for_nums[start_data[i] as usize] = i as u32;
     }
 
-    while turn < iterations {
-        if show_log && turn % log_rate == 0 {
-            println!("Progress: {} %", 100 * turn / iterations);
-        }
-
-        let prev_turn = turns_for_nums.get(&prev).map(|x| *x);
-        turns_for_nums.insert(prev, turn - 1);
+    while turn < iterations as u32 {
+        let prev_turn = turns_for_nums[prev as usize];
+        turns_for_nums[prev as usize] = turn - 1;
 
         prev = match prev_turn {
-            Some(i) => turn - i - 1,
-            None => 0,
+            std::u32::MAX => 0,
+            i => turn - i - 1,
         };
 
         turn += 1;
@@ -33,11 +29,11 @@ pub fn main() {
     let data = std::fs::read_to_string("data/day15.txt")
         .unwrap()
         .split(",")
-        .map(|x| x.parse::<usize>().unwrap())
+        .map(|x| x.parse::<u32>().unwrap())
         .collect::<Vec<_>>();
 
-    let part1 = run_game(2020, &data, false);
-    let part2 = run_game(30_000_000, &data, true);
+    let part1 = run_game(2020, &data);
+    let part2 = run_game(30_000_000, &data);
 
     println!("\n{} {}\n", part1, part2);
 }
