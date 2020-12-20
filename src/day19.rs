452264fec07442974b872e0159922e7cbbcb23cd
jaburns <j@jaburns.net>
2020-12-19 21:37:45 -0700

    Bit more day 19

--- src/day19.rs
+++ src/day19.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 type RuleId = u32;
 
@@ -16,24 +17,64 @@ enum RuleBody {
 
 impl Rule {
     pub fn parse(s: &str) -> Self {
-        let id_rest = s.split(":").collect::<Vec<_>>();
+        let id_rest = s.split(": ").collect::<Vec<_>>();
         let id = id_rest[0].parse::<u32>().unwrap();
+
+        fn parse_num_vec(s: &str) -> Vec<u32> {
+            s.split(" ")
+                .map(|y| y.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        }
 
         Self {
             id: id,
-            body: RuleBody::Value('a')
+            body: if id_rest[1].contains("|") {
+                let parts = id_rest[1]
+                    .split(" | ")
+                    .map(parse_num_vec)
+                    .collect::<Vec<_>>();
+                RuleBody::BranchSub(parts[0].clone(), parts[1].clone())
+            } else if id_rest[1].contains("\"") {
+                RuleBody::Value(id_rest[1].replace("\"", "").chars().last().unwrap())
+            } else {
+                RuleBody::SingleSub(parse_num_vec(id_rest[1]))
+            },
+        }
+    }
+
+    fn create_map_by_id(rules: &[Rule]) -> HashMap<RuleId, Rule> {
+        let mut ret = HashMap::new();
+
+        for rule in rules {
+            ret.insert(rule.id, rule.clone());
+        }
+
+        ret
+    }
+
+    fn evaluate(&self, rules_by_id: &HashMap<RuleId, Rule>) -> String {
+        match &self.body {
+            RuleBody::Value(chr) => String::from(*chr),
+            RuleBody::SingleSub(xs) => xs.iter().map(|x| rules_by_id[x].evaluate(rules_by_id)).collect::<Vec<_>>().join(""),
+            RuleBody::BranchSub(xs, ys) => String::new(),
         }
     }
 }
 
 pub fn main() {
-    let chunks = std::fs::read_to_string("data/day19.txt")
+    let chunks = std::fs::read_to_string("data/day19.test")
         .unwrap()
         .split("\n\n")
         .map(|x| x.lines().map(String::from).collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    let rules = chunks[0].iter().map(|x| Rule::parse(x.as_str())).collect::<Vec<_>>();
+    let rules = chunks[0]
+        .iter()
+        .map(|x| Rule::parse(x.as_str()))
+        .collect::<Vec<_>>();
 
-    println!("{:?}", rules);
+    let rules_by_id = Rule::create_map_by_id(&rules);
+    let part1 = rules_by_id[&0].evaluate( &rules_by_id );
+
+    println!("{:?}", part1);
 }
