65f39d56bc0f81078912431e9df0b8939bd07fca
jaburns <j@jaburns.net>
2020-12-23 17:58:05 -0700

    Day 23 from 1 hour to 0.3 s

--- src/day23.rs
+++ src/day23.rs
@@ -1,22 +1,47 @@
-use std::collections::{VecDeque};
+fn run_game(cups: &Vec<usize>, size: usize, iterations: usize) -> Vec<usize> {
+    let mut next_cup = vec![0usize; size + 1];
 
-fn step(cups: &mut VecDeque<u32>, max_val: u32) {
-    let cur = cups.pop_front().unwrap();
-    let hold0 = cups.pop_front().unwrap();
-    let hold1 = cups.pop_front().unwrap();
-    let hold2 = cups.pop_front().unwrap();
+    let mut prev_cup = if size > cups.len() {
+        size
+    } else {
+        cups[cups.len() - 1]
+    };
 
-    let mut dest = if cur == 1 { max_val } else { cur - 1 };
-    while dest == hold0 || dest == hold1 || dest == hold2 {
-        dest = if dest == 1 { max_val } else { dest - 1 };
+    for i in 0..size {
+        if i < cups.len() {
+            next_cup[prev_cup] = cups[i];
+            prev_cup = cups[i];
+        } else {
+            next_cup[prev_cup] = i + 1;
+            prev_cup = i + 1;
+        }
     }
 
-    cups.push_back(cur);
+    let mut cur_cup = cups[0];
+    for _ in 0..iterations {
+        let grab0 = next_cup[cur_cup];
+        let grab1 = next_cup[grab0];
+        let grab2 = next_cup[grab1];
 
-    let insert_idx = cups.iter().position(|&x| x == dest).unwrap();
-    cups.insert(insert_idx + 1, hold2);
-    cups.insert(insert_idx + 1, hold1);
-    cups.insert(insert_idx + 1, hold0);
+        let mut dest = if cur_cup == 1 { size } else { cur_cup - 1 };
+        while dest == grab0 || dest == grab1 || dest == grab2 {
+            dest = if dest == 1 { size } else { dest - 1 };
+        }
+
+        next_cup[cur_cup] = next_cup[grab2];
+        let old_dest_next = next_cup[dest];
+        next_cup[dest] = grab0;
+        next_cup[grab2] = old_dest_next;
+        cur_cup = next_cup[cur_cup];
+    }
+
+    let mut ret = Vec::new();
+    cur_cup = next_cup[1];
+    for _ in 0..8 {
+        ret.push( cur_cup);
+        cur_cup = next_cup[cur_cup];
+    }
+    ret
 }
 
 pub fn main() {
@@ -24,51 +49,19 @@ pub fn main() {
         .unwrap()
         .trim()
         .chars()
-        .map(|x| x.to_string().parse::<u32>().unwrap())
-        .collect::<VecDeque<_>>();
+        .map(|x| x.to_string().parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
 
-    let part1 = {
-        let mut cups = cups.clone();
-        for _ in 0..100 {
-            step(&mut cups, 9);
-        }
+    let part1 = run_game(&cups, 9, 100)
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join("");
 
-        while cups[0] != 1 {
-            cups.rotate_left(1);
-        }
-        cups.pop_front();
-
-        cups
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>()
-            .join("")
-    };
-
-    let part2: u64 = {
-        let mut cups = cups.clone();
-
-        for i in 10..=1_000_000 {
-            cups.push_back(i as u32);
-        }
-        for i in 0..10_000_000 {
-            if i % 1_000 == 0 {
-                println!("Percent done: {}", 100f32 * i as f32 / 10_000_000f32);
-            }
-            step(&mut cups, 1_000_000);
-        }
-
-        while cups[0] != 1 {
-            cups.rotate_left(1);
-        }
-
-        cups
-            .iter()
-            .skip(1)
-            .take(2)
-            .map(|&x| x as u64)
-            .product()
-    };
+    let part2 = run_game(&cups, 1_000_000, 10_000_000)
+        .iter()
+        .take(2)
+        .product::<usize>();
 
     println!("{} {}", part1, part2);
 }
