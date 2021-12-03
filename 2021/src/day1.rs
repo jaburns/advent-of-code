4cc24f5711262baaf7b9e4968150c7e30ded9353
jaburns <j@jaburns.net>
2021-12-03 15:08:03 -0700

    Fix day1

--- 2021/src/day1.rs
+++ 2021/src/day1.rs
@@ -17,17 +17,3 @@ pub fn part2(lines: &[&str], out: &mut String) {
 
     write!(out, "{}", result).unwrap();
 }
-
-use std::fmt::Write;
-
-pub fn part1(lines: &[&str], out: &mut String) {
-    let result = lines.len();
-
-    write!(out, "{}", result).unwrap();
-}
-
-pub fn part2(lines: &[&str], out: &mut String) {
-    let result = lines.len();
-
-    write!(out, "{}", result).unwrap();
-}
