15437413c56da2aa35bdf6f1f0f529459431d848
jaburns <j@jaburns.net>
2021-12-09 11:55:26 -0800

    Clean up day 8

--- 2021/src/day8.rs
+++ 2021/src/day8.rs
@@ -1,101 +1,6 @@
 use hashbrown::HashSet;
 use std::fmt::Write;
 
-fn decode_digits_from_notes(pattern_notes: &str) -> [HashSet<char>; 10] {
-    let mut mapping = [
-        HashSet::default(),
-        HashSet::default(),
-        HashSet::default(),
-        HashSet::default(),
-        HashSet::default(),
-        HashSet::default(),
-        HashSet::default(),
-        HashSet::default(),
-        HashSet::default(),
-        HashSet::default(),
-    ];
-
-    let mut sets = pattern_notes
-        .split(' ')
-        .map(|x| x.chars().collect::<HashSet<char>>())
-        .collect::<Vec<_>>();
-
-    fn find_remove<T>(vec: &mut Vec<T>, func: impl Fn(&T) -> bool) -> T {
-        let idx = vec.iter().position(func).unwrap();
-        vec.remove(idx)
-    }
-
-    mapping[1] = find_remove(&mut sets, |x| x.len() == 2);
-    mapping[4] = find_remove(&mut sets, |x| x.len() == 4);
-    mapping[7] = find_remove(&mut sets, |x| x.len() == 3);
-    mapping[8] = find_remove(&mut sets, |x| x.len() == 7);
-
-    mapping[3] = find_remove(&mut sets, |x| {
-        if x.len() != 5 {
-            return false;
-        }
-        for seg in mapping[1].iter() {
-            if !x.contains(seg) {
-                return false;
-            }
-        }
-        true
-    });
-    mapping[9] = find_remove(&mut sets, |x| {
-        if x.len() != 6 {
-            return false;
-        }
-        for seg in mapping[4].iter() {
-            if !x.contains(seg) {
-                return false;
-            }
-        }
-        true
-    });
-    mapping[0] = find_remove(&mut sets, |x| {
-        if x.len() != 6 {
-            return false;
-        }
-        for seg in mapping[7].iter() {
-            if !x.contains(seg) {
-                return false;
-            }
-        }
-        true
-    });
-    mapping[6] = find_remove(&mut sets, |x| x.len() == 6);
-
-    mapping[5] = find_remove(&mut sets, |x| {
-        for seg in x {
-            if !mapping[6].contains(seg) {
-                return false;
-            }
-        }
-        true
-    });
-
-    mapping[2] = find_remove(&mut sets, |_| true);
-
-    mapping
-}
-
-fn read_decoded_digit(mapping: &[HashSet<char>; 10], segments: &str) -> u32 {
-    let seg_chars: Vec<char> = segments.chars().collect();
-
-    'mapping_loop: for (i, map) in mapping.iter().enumerate() {
-        for chr in 'a'..='g' {
-            if seg_chars.contains(&chr) && !map.contains(&chr)
-                || !seg_chars.contains(&chr) && map.contains(&chr)
-            {
-                continue 'mapping_loop;
-            }
-        }
-        return i as u32;
-    }
-
-    panic!("Couldn't decode digit '{}'", segments);
-}
-
 pub fn part1(lines: &[&str], out: &mut String) {
     let result: usize = lines
         .iter()
@@ -123,17 +28,120 @@ pub fn part2(lines: &[&str], out: &mut String) {
         let output = halves.next().unwrap();
 
         let mapping = decode_digits_from_notes(notes);
-        let mut digits = output.split(' ').map(|x| read_decoded_digit(&mapping, x));
+        let digits = output.split(' ').map(|x| read_decoded_digit(&mapping, x));
 
-        let (d0, d1, d2, d3) = (
-            digits.next().unwrap(),
-            digits.next().unwrap(),
-            digits.next().unwrap(),
-            digits.next().unwrap(),
-        );
-
-        sum += d3 + 10 * d2 + 100 * d1 + 1000 * d0;
+        sum += digits
+            .rev()
+            .enumerate()
+            .fold(0_u32, |acc, (i, digit)| acc + 10_u32.pow(i as u32) * digit);
     }
 
     write!(out, "{}", sum).unwrap();
+}
+
+fn decode_digits_from_notes(pattern_notes: &str) -> [HashSet<char>; 10] {
+    let mut out_mapping = [
+        HashSet::default(),
+        HashSet::default(),
+        HashSet::default(),
+        HashSet::default(),
+        HashSet::default(),
+        HashSet::default(),
+        HashSet::default(),
+        HashSet::default(),
+        HashSet::default(),
+        HashSet::default(),
+    ];
+
+    let mut in_sets = pattern_notes
+        .split(' ')
+        .map(|x| x.chars().collect::<HashSet<char>>())
+        .collect::<Vec<_>>();
+
+    enum SegmentConstraint {
+        Count(usize),
+        SupersetOf(usize),
+        SubsetOf(usize),
+    }
+
+    let mut take_digit = |digit: usize, constraints: &[SegmentConstraint]| {
+        let idx = in_sets
+            .iter()
+            .position(|segs| {
+                for cons in constraints.iter() {
+                    match *cons {
+                        SegmentConstraint::Count(c) => {
+                            if segs.len() != c {
+                                return false;
+                            }
+                        }
+                        SegmentConstraint::SupersetOf(d) => {
+                            for seg in out_mapping[d].iter() {
+                                if !segs.contains(seg) {
+                                    return false;
+                                }
+                            }
+                        }
+                        SegmentConstraint::SubsetOf(d) => {
+                            for seg in segs.iter() {
+                                if !out_mapping[d].contains(seg) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+                true
+            })
+            .unwrap();
+        out_mapping[digit] = in_sets.remove(idx);
+    };
+
+    take_digit(1, &[SegmentConstraint::Count(2)]);
+    take_digit(4, &[SegmentConstraint::Count(4)]);
+    take_digit(7, &[SegmentConstraint::Count(3)]);
+    take_digit(8, &[SegmentConstraint::Count(7)]);
+    take_digit(
+        3,
+        &[
+            SegmentConstraint::Count(5),
+            SegmentConstraint::SupersetOf(1),
+        ],
+    );
+    take_digit(
+        9,
+        &[
+            SegmentConstraint::Count(6),
+            SegmentConstraint::SupersetOf(4),
+        ],
+    );
+    take_digit(
+        0,
+        &[
+            SegmentConstraint::Count(6),
+            SegmentConstraint::SupersetOf(7),
+        ],
+    );
+    take_digit(6, &[SegmentConstraint::Count(6)]);
+    take_digit(5, &[SegmentConstraint::SubsetOf(6)]);
+    take_digit(2, &[]);
+
+    out_mapping
+}
+
+fn read_decoded_digit(mapping: &[HashSet<char>; 10], segments: &str) -> u32 {
+    let seg_chars: Vec<char> = segments.chars().collect();
+
+    'mapping_loop: for (i, map) in mapping.iter().enumerate() {
+        for chr in 'a'..='g' {
+            if seg_chars.contains(&chr) && !map.contains(&chr)
+                || !seg_chars.contains(&chr) && map.contains(&chr)
+            {
+                continue 'mapping_loop;
+            }
+        }
+        return i as u32;
+    }
+
+    panic!("Couldn't decode digit '{}'", segments);
 }
