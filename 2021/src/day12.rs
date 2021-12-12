3c262c9c26c5c51355f03d6303e1ec2ba827838f
jaburns <j@jaburns.net>
2021-12-11 23:19:51 -0700

    Day 12 - part 2 done wrong

--- 2021/src/day12.rs
+++ 2021/src/day12.rs
@@ -40,38 +40,49 @@ impl<'a> CaveSystem<'a> {
 }
 
 #[derive(Debug)]
-struct CaveWalk<'a> {
+struct CaveWalk<'a, const MAX_VISITS: u32> {
     system: Rc<CaveSystem<'a>>,
-    blacklist: HashSet<&'a str>,
+    visited: HashMap<&'a str, u32>,
+    breadcrumbs: Vec<&'a str>,
     node: &'a str,
 }
 
-impl<'a> CaveWalk<'a> {
+impl<'a, const MAX_VISITS: u32> CaveWalk<'a, MAX_VISITS> {
     fn new(system: CaveSystem<'a>) -> Self {
         Self {
             system: Rc::new(system),
-            blacklist: HashSet::default(),
+            visited: HashMap::default(),
+            breadcrumbs: vec!["start"],
             node: "start",
         }
     }
 
-    fn step(mut self, out_walks: &mut Vec<CaveWalk<'a>>) -> bool {
+    fn step(mut self, walks_out: &mut Vec<CaveWalk<'a, MAX_VISITS>>) -> bool {
         if self.node == "end" {
+            println!("{:?}", self.breadcrumbs);
             return true;
         }
 
         if !self.system.big_caves.contains(self.node) {
-            self.blacklist.insert(self.node);
+            *self.visited.entry(self.node).or_insert(0) += 1;
         }
 
         for &next_node in &self.system.edges[self.node] {
-            if self.blacklist.contains(next_node) {
+            if next_node == "start" {
                 continue;
             }
 
-            out_walks.push(Self {
+            if *self.visited.get(next_node).unwrap_or(&0) >= MAX_VISITS {
+                continue;
+            }
+
+            let mut breadcrumbs = self.breadcrumbs.clone();
+            breadcrumbs.push(next_node);
+
+            walks_out.push(Self {
                 system: self.system.clone(),
-                blacklist: self.blacklist.clone(),
+                visited: self.visited.clone(),
+                breadcrumbs,
                 node: next_node,
             });
         }
@@ -80,45 +91,40 @@ impl<'a> CaveWalk<'a> {
     }
 }
 
-pub fn part1(lines: &[&str], out: &mut String) {
+fn find_paths<const MAX_VISITS: u32>(lines: &[&str]) -> u32 {
     let caves = CaveSystem::parse(lines);
 
-    let mut walks0 = vec![CaveWalk::new(caves)];
-    let mut walks1: Vec<CaveWalk> = vec![];
+    let mut walks = vec![CaveWalk::<MAX_VISITS>::new(caves)];
     let mut total_paths = 0_u32;
 
-    let mut buffer_flip = false;
-
     loop {
-        buffer_flip = !buffer_flip;
-        let (walks_from, walks_to) = if buffer_flip {
-            (&mut walks0, &mut walks1)
-        } else {
-            (&mut walks1, &mut walks0)
-        };
-
         let mut non_terminated_count = 0;
+        let mut walks_out = vec![];
 
-        for walk in walks_from.drain(0..walks_from.len()) {
-            if walk.step(walks_to) {
+        for walk in walks {
+            if walk.step(&mut walks_out) {
                 total_paths += 1;
             } else {
                 non_terminated_count += 1;
             }
         }
 
+        walks = walks_out;
+
         if non_terminated_count == 0 {
             break;
         }
     }
 
-    let result = total_paths;
+    total_paths
+}
 
+pub fn part1(lines: &[&str], out: &mut String) {
+    let result = find_paths::<1>(lines);
     write!(out, "{}", result).unwrap();
 }
 
 pub fn part2(lines: &[&str], out: &mut String) {
-    let result = lines.len();
-
+    let result = find_paths::<2>(lines);
     write!(out, "{}", result).unwrap();
 }
