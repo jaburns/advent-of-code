5dd05c07dd292c228a2fb4ddc1edde897cd23c34
Jeremy Burns <j@jaburns.net>
2021-12-15 10:30:45 -0700

    Reorder fns

--- 2021/src/day14.rs
+++ 2021/src/day14.rs
@@ -1,9 +1,38 @@
 use hashbrown::HashMap;
 use std::fmt::Write;
 
+pub fn part1(lines: &[&str], out: &mut String) {
+    simulate_chain_and_get_disparity(10, lines, out);
+}
+
+pub fn part2(lines: &[&str], out: &mut String) {
+    simulate_chain_and_get_disparity(40, lines, out);
+}
+
 type RuleSet = HashMap<(char, char), char>;
 
 type PairCounts = HashMap<(char, char), u64>;
+
+fn simulate_chain_and_get_disparity(iterations: u32, lines: &[&str], out: &mut String) {
+    let mut rules = RuleSet::default();
+    for line in lines[2..].iter() {
+        parse_rule(&mut rules, line);
+    }
+
+    let chars = lines[0].chars().collect::<Vec<_>>();
+
+    let mut counts = PairCounts::default();
+    for pair in chars.windows(2) {
+        *counts.entry((pair[0], pair[1])).or_insert(0) += 1;
+    }
+
+    for _ in 0..iterations {
+        apply_rules_to_pair_counts(&rules, &mut counts);
+    }
+
+    let result = get_max_element_count_difference(chars[0], &counts);
+    write!(out, "{}", result).unwrap();
+}
 
 fn parse_rule(rules: &mut RuleSet, line: &str) {
     let mut parts = line.split(" -> ");
@@ -40,33 +69,4 @@ fn get_max_element_count_difference(first_element: char, pair_counts: &PairCount
     let mut counts: Vec<u64> = counts.into_iter().map(|(_, x)| x).collect();
     counts.sort_unstable();
     counts[counts.len() - 1] - counts[0]
-}
-
-fn simulate_chain_and_get_disparity(iterations: u32, lines: &[&str], out: &mut String) {
-    let mut rules = RuleSet::default();
-    for line in lines[2..].iter() {
-        parse_rule(&mut rules, line);
-    }
-
-    let chars = lines[0].chars().collect::<Vec<_>>();
-
-    let mut counts = PairCounts::default();
-    for pair in chars.windows(2) {
-        *counts.entry((pair[0], pair[1])).or_insert(0) += 1;
-    }
-
-    for _ in 0..iterations {
-        apply_rules_to_pair_counts(&rules, &mut counts);
-    }
-
-    let result = get_max_element_count_difference(chars[0], &counts);
-    write!(out, "{}", result).unwrap();
-}
-
-pub fn part1(lines: &[&str], out: &mut String) {
-    simulate_chain_and_get_disparity(10, lines, out);
-}
-
-pub fn part2(lines: &[&str], out: &mut String) {
-    simulate_chain_and_get_disparity(40, lines, out);
 }
