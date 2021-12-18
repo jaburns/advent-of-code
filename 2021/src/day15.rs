1455c4fbf33aede189073467607a4c7de331993e
jaburns <j@jaburns.net>
2021-12-18 16:11:29 -0700

    Day 15 part 1

--- 2021/src/day15.rs
+++ 2021/src/day15.rs
@@ -1,102 +1,39 @@
-use hashbrown::HashSet;
+use pathfinding::prelude::astar;
 use std::fmt::Write;
 
-fn parse_cave(lines: &[&str]) -> Vec<Vec<u32>> {
-    // cave[row][col]
+fn parse_cave(lines: &[&str]) -> Vec<Vec<usize>> {
     lines
         .iter()
-        .map(|line| line.chars().map(|x| x.to_digit(10).unwrap()).collect())
+        .map(|line| {
+            line.chars()
+                .map(|x| x.to_digit(10).unwrap() as usize)
+                .collect()
+        })
         .collect()
-}
-
-#[derive(Clone)]
-struct Walk {
-    coord: (usize, usize),
-    visited: HashSet<(usize, usize)>,
-    total_risk: u32,
-}
-
-fn step_walks(cave: &[Vec<u32>], walks: &mut Vec<Walk>) -> Option<u32> {
-    let w = walks.remove(0);
-
-    if w.coord.0 == cave[0].len() - 1 && w.coord.1 == cave.len() - 1 {
-        return Some(w.total_risk);
-    }
-
-    if w.coord.0 < cave[0].len() - 1 {
-        let (x1, y1) = (w.coord.0 + 1, w.coord.1);
-        if !w.visited.contains(&(x1, y1)) {
-            let mut visited = w.visited.clone();
-            visited.insert((x1, y1));
-            walks.push(Walk {
-                coord: (x1, y1),
-                visited,
-                total_risk: w.total_risk + cave[y1][x1],
-            });
-        }
-    }
-    if w.coord.1 < cave.len() - 1 {
-        let (x1, y1) = (w.coord.0, w.coord.1 + 1);
-        if !w.visited.contains(&(x1, y1)) {
-            let mut visited = w.visited.clone();
-            visited.insert((x1, y1));
-            walks.push(Walk {
-                coord: (x1, y1),
-                visited,
-                total_risk: w.total_risk + cave[y1][x1],
-            });
-        }
-    }
-    if w.coord.0 > 0 {
-        let (x1, y1) = (w.coord.0 - 1, w.coord.1);
-        if !w.visited.contains(&(x1, y1)) {
-            let mut visited = w.visited.clone();
-            visited.insert((x1, y1));
-            walks.push(Walk {
-                coord: (x1, y1),
-                visited,
-                total_risk: w.total_risk + cave[y1][x1],
-            });
-        }
-    }
-    if w.coord.1 > 0 {
-        let (x1, y1) = (w.coord.0, w.coord.1 - 1);
-        if !w.visited.contains(&(x1, y1)) {
-            let mut visited = w.visited.clone();
-            visited.insert((x1, y1));
-            walks.push(Walk {
-                coord: (x1, y1),
-                visited,
-                total_risk: w.total_risk + cave[y1][x1],
-            });
-        }
-    }
-
-    walks.sort_unstable_by(|a, b| a.total_risk.cmp(&b.total_risk));
-
-    while walks.len() > 5000 {
-        walks.pop().unwrap();
-    }
-
-    None
 }
 
 pub fn part1(lines: &[&str], out: &mut String) {
     let cave = parse_cave(lines);
-    let mut visited = HashSet::new();
-    visited.insert((0, 0));
 
-    let mut walks = vec![Walk {
-        coord: (0, 0),
-        visited,
-        total_risk: 0,
-    }];
-
-    let result = loop {
-        if let Some(result) = step_walks(&cave, &mut walks) {
-            break result;
-        }
-    };
+    let result = astar(
+        &(0_usize, 0_usize),
+        |&(r, c)| {
+            vec![(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
+                .into_iter()
+                .filter_map(|(r, c)| {
+                    if r < cave.len() && c < cave[0].len() {
+                        Some(((r, c), cave[r][c]))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        },
+        |&(r, c)| (cave.len() - 1 - r) + (cave[0].len() - 1 - c),
+        |&(r, c)| r == cave.len() - 1 && c == cave[0].len() - 1,
+    )
+    .unwrap()
+    .1;
 
     write!(out, "{}", result).unwrap();
 }
