25953db08b70401b4aaa03f751ae8b35086117e1
jaburns <j@jaburns.net>
2021-12-28 23:53:26 -0700

    Day 20, Day 21 pt 1

--- 2021/src/day20.rs
+++ 2021/src/day20.rs
@@ -1,13 +1,94 @@
 use std::fmt::Write;
 
-pub fn part1(lines: &[&str], out: &mut String) {
-    let result = lines.len();
+const BORDER: usize = 100;
 
-    write!(out, "{}", result).unwrap();
+fn sample_grid(grid: &[Vec<bool>], mut row: isize, mut col: isize) -> bool {
+    if row < 0 {
+        row = 0;
+    }
+    if col < 0 {
+        col = 0;
+    }
+    if row >= grid.len() as isize {
+        row = grid.len() as isize - 1;
+    }
+    if col >= grid[0].len() as isize {
+        col = grid[0].len() as isize - 1;
+    }
+    grid[row as usize][col as usize]
+}
+
+fn run_rule_on_grid(rule: &[bool], grid: &mut Vec<Vec<bool>>) {
+    let mut new_grid = grid.clone();
+
+    for r in 0..grid.len() as isize {
+        for c in 0..grid[0].len() as isize {
+            let bits = [
+                sample_grid(grid, r - 1, c - 1),
+                sample_grid(grid, r - 1, c),
+                sample_grid(grid, r - 1, c + 1),
+                sample_grid(grid, r, c - 1),
+                sample_grid(grid, r, c),
+                sample_grid(grid, r, c + 1),
+                sample_grid(grid, r + 1, c - 1),
+                sample_grid(grid, r + 1, c),
+                sample_grid(grid, r + 1, c + 1),
+            ];
+
+            let val = (bits[0] as usize * 0b100000000)
+                | (bits[1] as usize * 0b010000000)
+                | (bits[2] as usize * 0b001000000)
+                | (bits[3] as usize * 0b000100000)
+                | (bits[4] as usize * 0b000010000)
+                | (bits[5] as usize * 0b000001000)
+                | (bits[6] as usize * 0b000000100)
+                | (bits[7] as usize * 0b000000010)
+                | bits[8] as usize;
+
+            new_grid[r as usize][c as usize] = rule[val];
+        }
+    }
+
+    grid.clone_from(&new_grid);
+}
+
+fn solve(lines: &[&str], out: &mut String, iter: u32) {
+    let mut grid = vec![];
+    let size = lines[2].len();
+
+    for _ in 0..BORDER {
+        grid.push(vec![false; size + 2 * BORDER]);
+    }
+    for line in lines.iter().skip(2) {
+        let mut cur_row = vec![false; BORDER];
+        for ch in line.chars() {
+            cur_row.push(ch == '#');
+        }
+        cur_row.resize(size + 2 * BORDER, false);
+        grid.push(cur_row);
+    }
+    for _ in 0..BORDER {
+        grid.push(vec![false; size + 2 * BORDER]);
+    }
+
+    let rule = lines[0].chars().map(|x| x == '#').collect::<Vec<_>>();
+
+    for _ in 0..iter {
+        run_rule_on_grid(&rule, &mut grid);
+    }
+
+    let count = grid
+        .iter()
+        .map(|x| x.iter().map(|x| *x as u32).sum::<u32>())
+        .sum::<u32>();
+
+    write!(out, "{}", count).unwrap();
+}
+
+pub fn part1(lines: &[&str], out: &mut String) {
+    solve(lines, out, 2);
 }
 
 pub fn part2(lines: &[&str], out: &mut String) {
-    let result = lines.len();
-
-    write!(out, "{}", result).unwrap();
+    solve(lines, out, 50);
 }
--- 2021/src/day21.rs
+++ 2021/src/day21.rs
@@ -1,7 +1,47 @@
 use std::fmt::Write;
 
 pub fn part1(lines: &[&str], out: &mut String) {
-    let result = lines.len();
+    let mut pos = [
+        lines[0]
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<u32>()
+            .unwrap(),
+        lines[1]
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<u32>()
+            .unwrap(),
+    ];
+    let mut score = [0_u32, 0_u32];
+    let mut die = 1_u32;
+    let mut player = 0_usize;
+    let mut rolls = 0_u32;
+
+    loop {
+        let mut mov = die;
+        die = (die % 100) + 1;
+        rolls += 1;
+        mov += die;
+        die = (die % 100) + 1;
+        rolls += 1;
+        mov += die;
+        die = (die % 100) + 1;
+        rolls += 1;
+
+        pos[player] = ((pos[player] + mov - 1) % 10) + 1;
+        score[player] += pos[player];
+
+        if score[player] >= 1000 {
+            break;
+        }
+
+        player = (player + 1) & 1;
+    }
+
+    let result = score[0].min(score[1]) * rolls;
 
     write!(out, "{}", result).unwrap();
 }
