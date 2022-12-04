55632613a9302acdfaa1f54cc5341a886caf3afb
jaburns <j@jaburns.net>
2022-12-04 16:49:18 -0700

    Day 4, 244 chars

--- 2022/crates/day_4/src/main.rs
+++ 2022/crates/day_4/src/main.rs
@@ -1,11 +1,13 @@
-// 256 characters
+// 244 characters
 #[allow(clippy::all)]
 #[rustfmt::skip]
 fn main() {
     let (mut m, mut n, mut v) = (0, 0, [0; 4]);
     for l in std::fs::read_to_string("I").unwrap().lines() {
-        for (i, x) in l.replace(",", "-").split("-").enumerate() {
-            v[i] = x.parse::<i32>().unwrap()
+        let mut i = 0;
+        for x in l.split(['-', ',']) {
+            v[i] = x.parse::<i32>().unwrap();
+            i += 1
         }
         let [a, b, c, d] = v;
         m += (a <= c && b >= d || a >= c && b <= d) as i32;
