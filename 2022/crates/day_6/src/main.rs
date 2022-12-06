df48b9bb3815fe005772d8a772069657959e5202
jaburns <j@jaburns.net>
2022-12-06 10:05:16 -0700

    Day 6, 146 chars

--- 2022/crates/day_6/src/main.rs
+++ 2022/crates/day_6/src/main.rs
@@ -1,18 +1,15 @@
-// 149 characters
+// 146 characters
 #[allow(clippy::all)]
 #[rustfmt::skip]
 fn main() {
-    for j in [4, 14] {
-        let mut i = j;
-        for w in std::fs::read("I").unwrap().windows(i) {
+    for mut i in [4, 14] {
+        std::fs::read("I").unwrap().windows(i).any(|w| {
+            i += 1;
             let mut x = w.to_vec();
             x.sort();
             x.dedup();
-            if x.len() == j {
-                dbg!(i);
-                break
-            }
-            i += 1
-        }
+            x.len() == w.len()
+        });
+        dbg!(i - 1);
     }
 }
