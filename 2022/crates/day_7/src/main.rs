a0ab5c6d4fbad42058d7aa3b0e99f88b72b7e917
jaburns <j@jaburns.net>
2022-12-07 19:38:28 -0700

    Day 7 part 1+2, 412 chars

--- 2022/crates/day_7/src/main.rs
+++ 2022/crates/day_7/src/main.rs
@@ -1,47 +1,27 @@
-// 602 characters
+// 412 characters
 #[allow(clippy::all)]
+#[rustfmt::skip]
 fn main() {
-    use {std::collections::HashMap as M, u64 as U};
-    struct T(M<String, T>, U);
-
-    unsafe {
-        let mut t = T(M::new(), 0);
-        let mut s = vec![&mut t as *mut T];
-
-        for l in std::fs::read_to_string("I").unwrap().lines() {
-            let i = s.len() - 1;
+    f(f(0) - 40000000);
+    fn f(q: u64) -> u64 {
+        let (mut s, mut v, mut x, mut y, mut d) = (0, vec![], 0, u64::MAX, 0);
+        let mut g = |l: &str| {
             if &l[4..] == " .." {
-                s.pop();
+                if s < 100001 { x += s }
+                if s >= q && s < y { y = s }
+                s += v.pop().unwrap()
             } else if &l[2..3] == "c" {
-                let c = (&mut *s[i])
-                    .0
-                    .entry(l[5..].into())
-                    .or_insert(T(M::new(), 0));
-                s.push(c as *mut _);
+                v.push(s);
+                s = 0
             }
             if &l[0..1] != "d" && &l[0..1] != "$" {
-                l.split_once(' ')
-                    .map(|p| (&mut *s[i]).1 += p.0.parse::<U>().unwrap());
+                l.split_once(' ').map(|p| s += p.0.parse::<u64>().unwrap());
             }
-        }
-
-        static mut S: (U, U, U) = (0, 40000000, U::MAX);
-
-        fn f(t: &T) -> U {
-            let z = t.1 + t.0.values().map(f).sum::<U>();
-            unsafe {
-                if z < 100001 {
-                    S.0 += z
-                }
-                if z > S.2 && z < S.1 {
-                    S.1 = z
-                }
-            }
-            z
-        }
-
-        S.2 = f(&t) - S.1;
-        f(&t);
-        dbg!(S.0 / 2, S.1);
+            v.len()
+        };
+        for l in std::fs::read_to_string("I").unwrap().lines() { d = g(l) }
+        for _ in 1..d { g("$    .."); }
+        if q > 0 { dbg!(x, y); }
+        s
     }
 }
