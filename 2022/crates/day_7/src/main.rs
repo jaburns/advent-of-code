bf84fabc9b737f42b4a8a4e1038925334e9a4f2e
jaburns <j@jaburns.net>
2022-12-07 19:01:02 -0700

    Day 7 part 1+2, 602 chars

--- 2022/crates/day_7/src/main.rs
+++ 2022/crates/day_7/src/main.rs
@@ -1,12 +1,11 @@
-// 650 characters
+// 602 characters
 #[allow(clippy::all)]
 fn main() {
     use {std::collections::HashMap as M, u64 as U};
-    #[derive(Default)]
-    struct T(M<String, T>, M<String, U>);
+    struct T(M<String, T>, U);
 
     unsafe {
-        let mut t = T::default();
+        let mut t = T(M::new(), 0);
         let mut s = vec![&mut t as *mut T];
 
         for l in std::fs::read_to_string("I").unwrap().lines() {
@@ -14,19 +13,22 @@ fn main() {
             if &l[4..] == " .." {
                 s.pop();
             } else if &l[2..3] == "c" {
-                let c = (&mut *s[i]).0.entry(l[5..].into()).or_default();
+                let c = (&mut *s[i])
+                    .0
+                    .entry(l[5..].into())
+                    .or_insert(T(M::new(), 0));
                 s.push(c as *mut _);
             }
             if &l[0..1] != "d" && &l[0..1] != "$" {
                 l.split_once(' ')
-                    .map(|(p, q)| (&mut *s[i]).1.insert(q.into(), p.parse().unwrap()));
+                    .map(|p| (&mut *s[i]).1 += p.0.parse::<U>().unwrap());
             }
         }
 
         static mut S: (U, U, U) = (0, 40000000, U::MAX);
 
         fn f(t: &T) -> U {
-            let z = t.1.values().sum::<U>() + t.0.values().map(f).sum::<U>();
+            let z = t.1 + t.0.values().map(f).sum::<U>();
             unsafe {
                 if z < 100001 {
                     S.0 += z
