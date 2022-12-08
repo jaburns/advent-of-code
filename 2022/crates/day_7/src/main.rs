ce98dca7b2bdca692616d1aaf3d8117e6b4cffee
jaburns <j@jaburns.net>
2022-12-07 19:53:50 -0700

    Day 7 part 1+2, 411 chars

--- 2022/crates/day_7/src/main.rs
+++ 2022/crates/day_7/src/main.rs
@@ -1,27 +1,28 @@
-// 412 characters
+// 411 characters
 #[allow(clippy::all)]
 #[rustfmt::skip]
 fn main() {
-    f(f(0) - 40000000);
-    fn f(q: u64) -> u64 {
-        let (mut s, mut v, mut x, mut y, mut d) = (0, vec![], 0, u64::MAX, 0);
+    type U = u64;
+    f(f(0, 0) - 40000000, U::MAX);
+    fn f(q: U, mut x: U) -> U {
+        let (mut s, mut v, mut d) = (0, vec![], 0);
         let mut g = |l: &str| {
             if &l[4..] == " .." {
-                if s < 100001 { x += s }
-                if s >= q && s < y { y = s }
+                if q < 1 && s < 100001 { x += s }
+                if q > 0 && s > q && s < x { x = s }
                 s += v.pop().unwrap()
             } else if &l[2..3] == "c" {
                 v.push(s);
                 s = 0
             }
             if &l[0..1] != "d" && &l[0..1] != "$" {
-                l.split_once(' ').map(|p| s += p.0.parse::<u64>().unwrap());
+                l.split_once(' ').map(|p| s += p.0.parse::<U>().unwrap());
             }
             v.len()
         };
         for l in std::fs::read_to_string("I").unwrap().lines() { d = g(l) }
         for _ in 1..d { g("$    .."); }
-        if q > 0 { dbg!(x, y); }
+        dbg!(x);
         s
     }
 }
