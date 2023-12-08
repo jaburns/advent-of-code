320ff15f2d6eb493a1f0e825f2ba02dbf0c522dc
jaburns <j@jaburns.net>
2023-12-08 14:57:19 -0700

    Day 3 no heap

--- 2023/src/day3.rs
+++ 2023/src/day3.rs
@@ -1,15 +1,23 @@
+use arrayvec::{ArrayString, ArrayVec};
 use glam::{ivec2, IVec2};
-use smallvec::SmallVec;
 use std::fmt::Write;
 
-type HashMap<K, V> =
-    std::collections::HashMap<K, V, core::hash::BuildHasherDefault<ahash::AHasher>>;
+const LINE_LEN: usize = 140;
+const LINE_COUNT: usize = 140;
+
+struct Gears {
+    set: [(u16, u16); LINE_LEN * LINE_COUNT],
+    sum: u64,
+}
 
 pub fn parts_1_and_2(lines: &[&str], out: &mut String) {
     let mut sum = 0_u64;
-    let mut gears = HashMap::<IVec2, SmallVec<[u32; 3]>>::default();
+    let mut gears: Gears = unsafe { std::mem::zeroed() };
 
-    let empty = ".".repeat(lines[0].len());
+    let mut empty = ArrayString::<LINE_LEN>::new();
+    for _ in 0..lines[0].len() {
+        empty.push('.');
+    }
 
     sum += sum_part_numbers(0, lines[0], &empty, lines[1], &mut gears);
     sum += sum_part_numbers(
@@ -23,14 +31,7 @@ pub fn parts_1_and_2(lines: &[&str], out: &mut String) {
         sum += sum_part_numbers(i as i32, lines[i], lines[i - 1], lines[i + 1], &mut gears);
     }
 
-    let mut gear_sum = 0_u64;
-    for vals in gears.into_values() {
-        if vals.len() == 2 {
-            gear_sum += (vals[0] * vals[1]) as u64;
-        }
-    }
-
-    write!(out, "{}  {}", sum, gear_sum).unwrap();
+    write!(out, "{}  {}", sum, gears.sum).unwrap();
 }
 
 #[allow(clippy::char_lit_as_u8)]
@@ -38,13 +39,7 @@ const DOT: u8 = '.' as u8;
 #[allow(clippy::char_lit_as_u8)]
 const GEAR: u8 = '*' as u8;
 
-fn sum_part_numbers(
-    line_idx: i32,
-    line: &str,
-    prev: &str,
-    next: &str,
-    gear_cache: &mut HashMap<IVec2, SmallVec<[u32; 3]>>,
-) -> u64 {
+fn sum_part_numbers(line_idx: i32, line: &str, prev: &str, next: &str, gears: &mut Gears) -> u64 {
     let mut sum = 0;
 
     let bytes = line.as_bytes();
@@ -54,7 +49,7 @@ fn sum_part_numbers(
     let mut digit_buf: [u8; 16] = Default::default();
     let mut digit_buf_idx: usize = 0;
     let mut valid = false;
-    let mut write_gears = SmallVec::<[IVec2; 8]>::default();
+    let mut write_gears = ArrayVec::<IVec2, 8>::default();
 
     for (i, ch) in line.chars().enumerate() {
         if ch.is_ascii_digit() {
@@ -104,8 +99,13 @@ fn sum_part_numbers(
                     .unwrap();
                 sum += parsed;
                 for gear in write_gears.drain(0..) {
-                    let list = gear_cache.entry(gear).or_default();
-                    list.push(parsed as u32);
+                    let entry = &mut gears.set[gear.x as usize + LINE_LEN * gear.y as usize];
+                    entry.0 += 1;
+                    if entry.0 == 2 {
+                        gears.sum += entry.1 as u64 * parsed;
+                    } else {
+                        entry.1 = parsed as u16;
+                    }
                 }
             }
             digit_buf_idx = 0;
@@ -119,8 +119,13 @@ fn sum_part_numbers(
             .unwrap();
         sum += parsed;
         for gear in write_gears.drain(0..) {
-            let list = gear_cache.entry(gear).or_default();
-            list.push(parsed as u32);
+            let entry = &mut gears.set[gear.x as usize + LINE_LEN * gear.y as usize];
+            entry.0 += 1;
+            if entry.0 == 2 {
+                gears.sum += entry.1 as u64 * parsed;
+            } else {
+                entry.1 = parsed as u16;
+            }
         }
     }
 
