3c769e2abd0cbdfdb738d9699b88d7796cf80d91
jaburns <j@jaburns.net>
2023-12-07 20:15:18 -0700

    Day 7 faster

--- 2023/src/day7.rs
+++ 2023/src/day7.rs
@@ -72,7 +72,7 @@ fn score_hand<const JOKERS: bool>(mut cards: [u8; 5]) -> u64 {
     let mut score_bytes = [0_u8; 8];
     score_bytes[3..].copy_from_slice(&cards);
 
-    cards.sort_unstable();
+    bose_nelson_sort_5(&mut cards);
 
     let mut last_card = 255_u8;
     let mut run_length = 0_usize;
@@ -117,4 +117,17 @@ fn score_hand<const JOKERS: bool>(mut cards: [u8; 5]) -> u64 {
     };
 
     u64::from_be_bytes(score_bytes)
+}
+
+#[rustfmt::skip]
+fn bose_nelson_sort_5(arr: &mut [u8; 5]) {
+    if arr[0] > arr[1] { arr.swap(0, 1); }
+    if arr[3] > arr[4] { arr.swap(3, 4); }
+    if arr[2] > arr[4] { arr.swap(2, 4); }
+    if arr[2] > arr[3] { arr.swap(2, 3); }
+    if arr[1] > arr[4] { arr.swap(1, 4); }
+    if arr[0] > arr[3] { arr.swap(0, 3); }
+    if arr[0] > arr[2] { arr.swap(0, 2); }
+    if arr[1] > arr[3] { arr.swap(1, 3); }
+    if arr[1] > arr[2] { arr.swap(1, 2); }
 }
