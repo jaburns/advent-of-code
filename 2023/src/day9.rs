6eec88684af0441a76952ce91944a2e6940e006c
jaburns <j@jaburns.net>
2023-12-09 10:42:23 -0700

    Use less memory day 9

--- 2023/src/day9.rs
+++ 2023/src/day9.rs
@@ -1,53 +1,51 @@
 use arrayvec::ArrayVec;
-use std::{fmt::Write, mem::zeroed};
+use glam::{ivec2, IVec2};
+use std::{fmt::Write, mem::swap};
 
 const HISTORY_BUFFER_CAP: usize = 32;
 
 pub fn parts_1_and_2(lines: &[&str], out: &mut String) {
-    let (result_1, result_2) = lines
+    let results = lines
         .iter()
         .map(|x| parse_and_extrapolate::<false>(x))
-        .reduce(|(x0, y0), (x1, y1)| (x0 + x1, y0 + y1))
-        .unwrap();
-    write!(out, "{}  {}", result_1, result_2).unwrap();
+        .sum::<IVec2>();
+    write!(out, "{}  {}", results.x, results.y).unwrap();
 }
 
-fn parse_and_extrapolate<const REVERSE: bool>(line: &str) -> (i32, i32) {
-    let mut diffs: [ArrayVec<i32, HISTORY_BUFFER_CAP>; HISTORY_BUFFER_CAP] = unsafe { zeroed() };
-
-    line.split_whitespace()
+fn parse_and_extrapolate<const REVERSE: bool>(line: &str) -> IVec2 {
+    let mut buffer_0 = line
+        .split_whitespace()
         .map(|x| x.parse::<i32>().unwrap())
-        .collect_into(&mut diffs[0]);
+        .collect::<ArrayVec<i32, HISTORY_BUFFER_CAP>>();
+    let mut buffer_1 = ArrayVec::<i32, HISTORY_BUFFER_CAP>::new();
 
-    let mut depth = 0_i32;
+    let mut acc = ArrayVec::<IVec2, HISTORY_BUFFER_CAP>::new();
+    acc.push(ivec2(*buffer_0.last().unwrap(), buffer_0[0]));
+
+    let mut cur = &mut buffer_0;
+    let mut next = &mut buffer_1;
+
     loop {
-        depth += 1;
-        let (upper, lower) = diffs.split_at_mut(depth as usize);
-        let prev = upper.last().unwrap();
-        let cur = lower.first_mut().unwrap();
-
         let mut all_zero = true;
-        for &[a, b] in prev.array_windows::<2>() {
-            let diff = b - a;
+        let mut diff = 0;
+        for &[a, b] in cur.array_windows::<2>() {
+            diff = b - a;
             all_zero &= diff == 0;
-            cur.push(diff);
+            next.push(diff);
         }
-
         if all_zero {
             break;
         }
+        acc.push(ivec2(diff, next[0]));
+        swap(&mut cur, &mut next);
+        next.clear();
     }
 
-    depth -= 1;
-
-    let mut future = 0_i32;
-    let mut past = 0_i32;
-
-    while depth >= 0 {
-        future += *diffs[depth as usize].last().unwrap();
-        past = *diffs[depth as usize].first().unwrap() - past;
-        depth -= 1;
+    let mut ret = IVec2::ZERO;
+    for v in acc.iter().rev() {
+        ret.x += v.x;
+        ret.y = v.y - ret.y;
     }
 
-    (future, past)
+    ret
 }
