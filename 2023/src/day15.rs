3dcaeed25ecf9c7670d644e988cdf18934670ff2
jaburns <j@jaburns.net>
2023-12-15 12:46:19 -0700

    Day 15 part 1

--- 2023/src/day15.rs
+++ 2023/src/day15.rs
@@ -1,7 +1,23 @@
 use std::fmt::Write;
 
 pub fn parts_1_and_2(lines: &[&str], out: &mut String) {
-    let result_0 = lines.len();
+    let input = lines[0];
+
+    let mut result_0 = 0;
+    for chunk in input.split(',') {
+        result_0 += hash(chunk.as_bytes())
+    }
+
     let result_1 = 0;
+
     write!(out, "{}  {}", result_0, result_1).unwrap();
+}
+
+fn hash(bytes: &[u8]) -> u64 {
+    let mut hash = 0_u8;
+    for b in bytes {
+        hash = hash.wrapping_add(*b);
+        hash = hash.wrapping_mul(17);
+    }
+    hash as u64
 }
