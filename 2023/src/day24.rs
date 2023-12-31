e8651db5c295c70fdf551825c11793bcaaed8384
jaburns <j@jaburns.net>
2023-12-30 19:37:13 -0700

    Day 24

--- 2023/src/day24.rs
+++ 2023/src/day24.rs
@@ -1,28 +1,25 @@
 use arrayvec::ArrayVec;
-use derive_more::{Add, AddAssign};
-use glam::{dvec2, dvec3, dvec4, vec3a, DVec2, DVec3, DVec4, Vec3A};
+use glam::{dvec2, dvec3, DVec2, DVec3};
+use nalgebra::{DMatrix, DVector};
 use std::fmt::Write;
 
 const MAX_LINES: usize = 300;
 
 #[derive(Default, Debug, Copy, Clone)]
 struct Line2 {
-    pt: DVec2,
-    dir: DVec2,
+    pos: DVec2,
+    vel: DVec2,
 }
 
 #[derive(Default, Debug, Copy, Clone)]
-struct Line4 {
-    pt: DVec4,
-    dir: DVec4,
+struct Line3 {
+    pos: DVec3,
+    vel: DVec3,
 }
-
-#[derive(Default, Debug, Copy, Clone, Add, AddAssign)]
-struct LVec3(i64, i64, i64);
 
 pub fn parts_1_and_2(inputs: &[&str], out: &mut String) {
     let mut lines2 = ArrayVec::<Line2, MAX_LINES>::new();
-    let mut lines4 = ArrayVec::<Line4, MAX_LINES>::new();
+    let mut lines3 = ArrayVec::<Line3, MAX_LINES>::new();
 
     for input in inputs {
         let mut sides = input.split(" @ ");
@@ -30,12 +27,12 @@ pub fn parts_1_and_2(inputs: &[&str], out: &mut String) {
             .next()
             .unwrap()
             .split(", ")
-            .map(|x| x.trim_start().parse::<i64>().unwrap());
+            .map(|x| x.trim_start().parse::<f64>().unwrap());
         let mut rights = sides
             .next()
             .unwrap()
             .split(", ")
-            .map(|x| x.trim_start().parse::<i64>().unwrap());
+            .map(|x| x.trim_start().parse::<f64>().unwrap());
         let pt = (
             lefts.next().unwrap(),
             lefts.next().unwrap(),
@@ -47,12 +44,12 @@ pub fn parts_1_and_2(inputs: &[&str], out: &mut String) {
             rights.next().unwrap(),
         );
         lines2.push(Line2 {
-            pt: dvec2(pt.0 as f64, pt.1 as f64),
-            dir: dvec2(dir.0 as f64, dir.1 as f64),
+            pos: dvec2(pt.0, pt.1),
+            vel: dvec2(dir.0, dir.1),
         });
-        lines4.push(Line4 {
-            pt: dvec4(pt.0 as f64, pt.1 as f64, pt.2 as f64, 0.0),
-            dir: dvec4(dir.0 as f64, dir.1 as f64, dir.2 as f64, 1.0),
+        lines3.push(Line3 {
+            pos: dvec3(pt.0, pt.1, pt.2),
+            vel: dvec3(dir.0, dir.1, dir.2),
         });
     }
 
@@ -75,93 +72,54 @@ pub fn parts_1_and_2(inputs: &[&str], out: &mut String) {
         }
     }
 
-    let mut test_pos = DVec3::ZERO;
-    let mut test_vel = DVec3::ZERO;
-
-    let measure_avg_distance = |pos: DVec3, vel: DVec3| -> f64 {
-        let test = line4(pos.extend(0.0), vel.extend(1.0));
-        let mut result = 0.0;
-        for (i, line) in lines4.iter().enumerate() {
-            result += min_dist_sqr(*line, test).sqrt() / lines4.len() as f64;
-        }
-        result
-    };
-
-    loop {
-        let base_dist = measure_avg_distance(test_pos, test_vel);
-
-        let mut dx = base_dist - measure_avg_distance(test_pos + DVec3::X, test_vel);
-        let mut dy = base_dist - measure_avg_distance(test_pos + DVec3::Y, test_vel);
-        let mut dz = base_dist - measure_avg_distance(test_pos + DVec3::Z, test_vel);
-        dx *= base_dist / 1_000.0;
-        dy *= base_dist / 1_000.0;
-        dz *= base_dist / 1_000.0;
-        test_pos += dvec3(dx, dy, dz);
-
-        let mut dx = (base_dist - measure_avg_distance(test_pos, test_vel + DVec3::X));
-        let mut dy = (base_dist - measure_avg_distance(test_pos, test_vel + DVec3::Y));
-        let mut dz = (base_dist - measure_avg_distance(test_pos, test_vel + DVec3::Z));
-        dx /= base_dist;
-        dy /= base_dist;
-        dz /= base_dist;
-        test_vel += dvec3(dx, dy, dz);
-
-        test_vel = test_vel.clamp(DVec3::splat(-500.0), DVec3::splat(500.0));
-
-        println!("{}  -  {} {}", base_dist, test_pos, test_vel);
-    }
-
-    let result_1 = 0;
-
-    // 163801440047383.16
-
-    write!(out, "{}  {:?}", result_0, result_1).unwrap();
+    let part_2_line = solve_part_2_line((lines3[0], lines3[1], lines3[2]));
+    let result_1 = (part_2_line.pos.x + part_2_line.pos.y + part_2_line.pos.z).round();
+    write!(out, "{}  {}", result_0, result_1).unwrap();
 }
 
-fn line4(pt: DVec4, dir: DVec4) -> Line4 {
-    Line4 { pt, dir }
-}
+fn solve_part_2_line(lines: (Line3, Line3, Line3)) -> Line3 {
+    let a = lines.1.vel - lines.0.vel;
+    let b = lines.0.pos - lines.1.pos;
+    let c = lines.1.pos.cross(lines.1.vel) - lines.0.pos.cross(lines.0.vel);
+    let d = lines.2.vel - lines.1.vel;
+    let e = lines.1.pos - lines.2.pos;
+    let f = lines.2.pos.cross(lines.2.vel) - lines.1.pos.cross(lines.1.vel);
 
-fn min_dist_sqr(line0: Line4, line1: Line4) -> f64 {
-    let a = line0.pt;
-    let b = line0.dir;
-    let c = line1.pt;
-    let d = line1.dir;
-    let e = a - c;
+    #[rustfmt::skip]
+    let (mat, rhs) = (
+        DMatrix::from_row_slice(6, 6, &[
+             0.0,  a.z, -a.y,  0.0,  b.z, -b.y,
+            -a.z,  0.0,  a.x, -b.z,  0.0,  b.x,
+             a.y, -a.x,  0.0,  b.y, -b.x,  0.0,
+             0.0,  d.z, -d.y,  0.0,  e.z, -e.y,
+            -d.z,  0.0,  d.x, -e.z,  0.0,  e.x,
+             d.y, -d.x,  0.0,  e.y, -e.x,  0.0,
+        ]),
+        DVector::from_column_slice(&[
+            c.x, c.y, c.z, f.x, f.y, f.z,
+        ])
+    );
 
-    let bb = b.dot(b);
-    let dd = d.dot(d);
-    let bd = b.dot(d);
-    let be = b.dot(e);
-    let de = d.dot(e);
+    let solution = mat.lu().solve(&rhs).unwrap();
 
-    let det = bd * bd - bb * dd;
-
-    if det.abs() < 1e-9 {
-        return line0.pt.distance_squared(line1.pt);
+    Line3 {
+        pos: DVec3::from_slice(&solution.data.as_slice()[0..3]),
+        vel: DVec3::from_slice(&solution.data.as_slice()[3..6]),
     }
-
-    let t0 = (dd * be - de * bd) / det;
-    let t1 = (be * bd - bb * de) / det;
-
-    let p0 = line0.pt + t0 * line0.dir;
-    let p1 = line1.pt + t1 * line1.dir;
-
-    p0.distance_squared(p1)
 }
 
 fn find_intersection(a: Line2, b: Line2) -> Option<DVec2> {
-    let det = a.dir.perp_dot(b.dir);
+    let det = a.vel.perp_dot(b.vel);
     if det.abs() < f64::EPSILON {
         return None;
     }
 
-    let b_from_a = b.pt - a.pt;
-    let t0 = (b_from_a).perp_dot(b.dir) / det;
-    let t1 = (-b_from_a).perp_dot(a.dir) / (-det);
+    let b_from_a = b.pos - a.pos;
+    let t0 = (b_from_a).perp_dot(b.vel) / det;
+    let t1 = (-b_from_a).perp_dot(a.vel) / (-det);
 
     if t0 > 0.0 && t1 > 0.0 {
-        Some(a.pt + t0 * a.dir)
+        Some(a.pos + t0 * a.vel)
     } else {
         None
     }
