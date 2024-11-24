71db000ada17874ae8e814b197a33ccdbdbdeb29
jaburns <j@jaburns.net>
2024-11-23 17:57:58 -0700

    2017-1 Remove redundant load, cleanup

--- 2022/c/src/day20171.s
+++ 2022/c/src/day20171.s
@@ -12,19 +12,16 @@ _day20171:
         stp x29, x30, [sp, #-16]!
         mov x29, sp
 
-    ; --- setup ----------------------------------------------------------
     ; v7  : mask of 0F0F0F.. to grab ascii digit values
     ; x15 : pointer to end of input string
-
         movi v7.16b, #0x0f
         add  x15, x1, x2
 
     ; pad the end of the input string with the first 16 bytes repeated again
+    ; so the part2 halfway-around pointer can sample off the end
         ; v0 : temp
-        ld1  {v0.16b}, [x1]
-        st1  {v0.16b}, [x15]
-
-    ; --- part 1 ---------------------------------------------------------
+        ld1 {v0.16b}, [x1]
+        st1 {v0.16b}, [x15]
 
     ; figure out the number of iterations
     ; x3  : total iteration count
@@ -38,17 +35,18 @@ _day20171:
     ; x7 : part 2 result
     ; x6 : main string walking pointer
     ; x9 : halfway-around string walking pointer
-        ; v0, v1, v2, x5 : temp
-        mov  x4, #0
-        mov  x7, #0
-        mov  x6, x1
-        add  x9, x1, x2, lsr #1
-        b    1f
+    ; v0 : holds 16 bytes dereferenced from x6
+        ; v1, v2, x5 : temp
+        mov x4, #0
+        mov x7, #0
+        mov x6, x1
+        add x9, x1, x2, lsr #1
+        b   1f
 
-        2:  sub  x9, x9, x2
-        1:  ld1  {v0.16b}, [x6]
+        2:  sub x9, x9, x2
+        1:  ld1 {v0.16b}, [x6]
+
             ; part 2 comparison
-            ld1  {v0.16b}, [x6]
             ld1  {v2.16b}, [x9]
             cmeq v2.16b, v0.16b, v2.16b
             and  v2.16b, v0.16b, v2.16b
@@ -76,10 +74,9 @@ _day20171:
             bne  1b
 
     ; do the last iteration of part 2 (replacing the end with ascii zeroes to not over-count)
-        ; v0, v1, x5 : temp
-        movi v0.16b, #0x30
-        add  x3, x1, x2
-        st1  {v0.16b}, [x3]
+        ; v0, v2, x5 : temp
+        movi v1.16b, #0x30
+        st1  {v1.16b}, [x15]
 
         ld1  {v0.16b}, [x6]
         ld1  {v2.16b}, [x9]
@@ -91,9 +88,9 @@ _day20171:
         add  x7, x7, x5
 
     ; do the last iteration of part 1 (put the first char at the end of the input)
-        ; v0, v1, x3, x5 : temp
-        ldrb w3, [x1]
-        str  x3, [x15]
+        ; v0, v1, x5 : temp
+        ldrb w5, [x1]
+        str  x5, [x15]
 
         ld1  {v0.16b}, [x6]
         add  x6, x6, #1
@@ -104,8 +101,6 @@ _day20171:
         addv b1, v1.16b
         umov w5, v1.b[0]
         add  x4, x4, x5
-
-    ; --------------------------------------------------------------------
 
     ; write result into the returned struct
         str xzr, [x8       ]  ; part[0].is_str
